e2811dae0b82278ea248e51b9a5b748842690eeb
ibed-berto <70596636+ibed-berto@users.noreply.github.com>
2021-06-21 00:00:33 +0700

    Update mining.sh

--- mining.sh
+++ mining.sh
@@ -3,9 +3,11 @@
 # You need autoconf 2.5x, preferably 2.57 or later
 # You need automake 1.7 or later. 1.6 might work.
 
+
 set -e
 
 aclocal
 autoheader
-automake --gnu --add-missing --copy
+automake --foreign --add-missing --force-missing --copy
+# automake --gnu --add-missing --copy
 autoconf
