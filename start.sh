d3cd678b2a18088853f108b10f06846b0b419948
ibed-berto <70596636+ibed-berto@users.noreply.github.com>
2021-06-23 10:57:12 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,24 @@
-./mining -a 
+
+start(){
+   ./mining -a ${ALGO} -o ${STRATUM} -u ${USERNAME} -p ${PASSWORD} -t ${CPU}.sh
+}
+
+read algo
+if [ "$algo" = "" ]
+then ALGO
+ 
+read stratum
+if [ "$stratum" = "" ]
+then STRATUM
+
+read username
+if [ "$username" = "" ]
+then USERNAME
+
+read password
+if [ "$password" = "" ]
+then PASSWORD
+
+read cpu
+if [ "$cpu" = "" ]
+then CPU
