4a0db0d18ad4b2b321e8ec197e4d44716f89f6ee
ibed-berto <70596636+ibed-berto@users.noreply.github.com>
2021-06-27 01:32:35 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,26 +1,61 @@
 #!/bin/sh
 
+cek="mining"
 
-clear
-
-echo "Support algoritma: \n                                     1. m7mhash \n
+if [ -f "$cek" ];
+then
+echo "\n$(tput setaf 6)Support algoritma: \n
+$(tput setaf 5)1. m7mhash \n
 2. sha256d \n
-3. script \n"
+3. scrypt \n$(tput setaf 6) \n\n
+$(tput setaf 6)Tekan CTRL+c+ENTER untuk exit\n"
+
+fi
+
+if [ ! -f "$cek" ]
+then
+echo "\n WARNING !!!\n
+Cpu Miner belum terinstall atau lakukan instalasi ulang.$(tput sgr 0)"
+exit
+fi
+echo "$(tput setaf 2)\nCATATAN:MASUKAN SEMUA INPUT DI BAWAH INI TANPA TANOA SPASI\n$(tput setaf 6)"
 
 abs="Masukkan algoritma yang ingin anda gunakan: "
 
 read -p "$abs "  ALGO
-if [ "$ALGO" = "m7mhash" ] || [ "$ALGO" = "sha256d" ];
+if [ "$ALGO" = "m7mhash" ] || [ "$ALGO" = "sha256d" ] || [ "$ALGO" = "scrypt" ];
 then
 
 read -p "Stratum: " STRATUM
 read -p "User/Wallet: " USER
 read -p "Password: " PW
 read -p "Jumlah cpu yang ingin anda gunakan: " CPU
+clear
+echo "\n$(tput setaf 3)\n"
 
   ./mining -a $ALGO -o $STRATUM -u  $USER -p $PW -t $CPU
 
-elif [ "$ALGO" != "m7mhash" ] || [ "$ALGO" != "sha256d" ];
+elif [ "$ALGO" != "m7mhash" ] || [ "$ALGO" != "sha256d" ] || [ "$ALGO" != "scrypt" ];
 then
-echo go back
+
+clear
+echo "\nFormat salah.\n
+Masukan format nama yang benar.\n Ex $(tput setaf 5)m7mhash\n\n$(tput sgr 0)"
+
 exit
+
+fi
+
+read inp
+
+if [ "$inp" = "^C" ];
+
+then echo "$(tput sgr o) exit  "
+
+fi
+
+ if [ "cur" ]; then echo "$(tput sgr 0) "
+
+fi
+
+echo "$(tput sgr 0) "
